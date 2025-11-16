eacc358845654fc0f1106b3312adf9da16cc3b79
shriyojstha <shriyojstha@gmail.com>
2025-11-16 17:22:21 +0545

    change final -- deployment

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendDist));
 
   // Catch-all must be '/*'
-  app.get("/:any(*)", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(frontendDist, "index.html"));
   });
 }
